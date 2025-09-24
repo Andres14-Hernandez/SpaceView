5f23e294533e4b3934f084a202a3ca1cfe43452e
Andres14-Hernandez <andres14_hl@hotmail.com>
2025-09-23 20:14:28 -0500

    V2.10

--- src/Pages/APOD/index.js
+++ src/Pages/APOD/index.js
@@ -1,23 +1,29 @@
 import styles from './APOD.module.css';
 import useFetch from '../../Assets/useAPOD'
 
-
 function APOD(){
 
     const { data, loading, error } = useFetch()
 
     return(
-        <section>
-            <h1 className={styles.title}>Astronomy Picture of the Day</h1>
+        <main className={styles.APOD}>
+            <h1>Astronomy Picture of the Day</h1>
+            <p>The universe hide secrets than only reveal who take a new look</p>
 
-            {loading && <p>Loading...</p>}
-            {error && <p>Error: {error}</p>}
+            <section className={styles.container}>
+                {loading && <p>Loading...</p>}
+                {error && <p>Error: {error}</p>}
 
-            <div className={styles.APODcontainer}>
-                <img src={data.url} alt={data.title}/>
-                <p> {data.explanation} </p>
-            </div>
-        </section>
+                <figure className={styles.imgContainer}>
+                    <img src={data.url} alt={data.title}/>
+                </figure>
+
+                <article className={styles.textContainer}>
+                    <h2>{data.title}</h2>
+                    <p> {data.explanation} </p>
+                </article>
+                </section>
+        </main>
     )
 } 
 
