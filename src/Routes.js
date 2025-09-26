0e8908887de8284959668041750c8072c490888b
Andres14-Hernandez <andres14_hl@hotmail.com>
2025-09-26 17:12:50 -0500

    Fix Scroll

--- src/Routes.js
+++ src/Routes.js
@@ -5,10 +5,12 @@ import NotFound from "./Pages/NotFound";
 import Gallery from "./Pages/Gallery";
 import APOD from "./Pages/APOD";
 import Articles from "./Pages/Articles";
+import ScrollToTop from "./Components/ScrollToTop";
 
 function AppRoutes(){
     return(
         <BrowserRouter>
+            <ScrollToTop/>
             <Routes>
                 <Route path="/" element={ <BasePage/> }>
                     <Route index="/home" element={ <Home/> }/>
