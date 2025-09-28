e0f1fe0d522f7d75a8e30ba599b2cf46f7c43e42
Andres14-Hernandez <andres14_hl@hotmail.com>
2025-09-28 16:19:01 -0500

    Not found page styles

--- src/Pages/NotFound/index.js
+++ src/Pages/NotFound/index.js
@@ -1,6 +1,18 @@
+import { Link } from 'react-router-dom';
+import styles from './NotFound.module.css';
+
 function NotFound(){
     return(
-        <h1>Not Found</h1>
+        <main className={styles.notFound}>
+            <h1>Not Found</h1>
+             <p>Oops! The page you're looking for doesn't exist.</p>
+
+             <Link to="/" className={styles.homeButton}>
+                Go back home
+            </Link>
+        </main>
+        
+        
     )
 }
 
